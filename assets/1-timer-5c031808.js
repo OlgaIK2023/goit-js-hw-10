f394eafff34e088d6a1adb293534f8efaf1cb938
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-02-04 14:05:15 +0000

    Deploying to gh-pages from @ OlgaIK2023/goit-js-hw-10@9222d28d7dba341d48109414dec5173fc365548f 🚀

--- assets/1-timer-5c031808.js
+++
@@ -1,2 +0,0 @@
-import"./vendor-18b38a61.js";
-//# sourceMappingURL=1-timer-5c031808.js.map
---
+++ assets/1-timer-ab2a42dc.js
@@ -0,0 +1,2 @@
+import"./vendor-94c342ae.js";function n(o){const t=Math.floor(o/864e5),c=Math.floor(o%864e5/36e5),s=Math.floor(o%864e5%36e5/6e4),e=Math.floor(o%864e5%36e5%6e4/1e3);return{days:t,hours:c,minutes:s,seconds:e}}console.log(n(2e3));console.log(n(14e4));console.log(n(2414e4));
+//# sourceMappingURL=1-timer-ab2a42dc.js.map
