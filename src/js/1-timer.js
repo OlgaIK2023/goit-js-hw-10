7d8e944a76193dff4ab841030d1f37e72c1c541c
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-02-04 16:25:07 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,41 +1,152 @@
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 
-import "izitoast/dist/css/iziToast.min.css";
-
+import 'izitoast/dist/css/iziToast.min.css';
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
 
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+document.body.style.backgroundColor = '#fff';
+const TIMER_DELAY = 1000;
+let intervalId = null;
+let selectedDate = null;
+let currentDate = null;
+
+const calendar = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+
+startBtn.disabled = true;
+
+iziToast.show 
+
+({
+    title: 'Greeting',
+    message: 'Please, choose a date and click on start',
+  });
+
+
+flatpickr(calendar, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() < Date.now()) {
+      iziToast.show({
+        title: 'Failure',
+        message: 'Please, choose a date in the future',
+      });
+    } else {
+      iziToast.show 
+      ({
+        title: 'Success',
+        message: 'Congratulation! Click on start!',
+      });
+      
+      
+      startBtn.disabled = false;
+      const setTimer = () => {
+        selectedDate = selectedDates[0].getTime();
+        timer.start();
+      };
+
+      startBtn.addEventListener('click', setTimer);
+    }
+  },
+});
+
+const timer = {
+  rootSelector: document.querySelector('.timer'),
+  start() {
+    intervalId = setInterval(() => {
+      startBtn.disabled = true;
+      calendar.disabled = true;
+      currentDate = Date.now();
+      const delta = selectedDate - currentDate;
+
+      if (delta <= 0) {
+        this.stop();
+        iziToast.show
+        
+        ({
+            title: 'Timer stopped!',
+            message: 'Please, if you want to start timer, choose a date and click on start or reload this page',
+          });
+
+
+          return;
+      }
+      const { days, hours, minutes, seconds } = this.convertMs(delta);
+      this.rootSelector.querySelector('[data-days]').textContent =
+        this.addLeadingZero(days);
+      this.rootSelector.querySelector('[data-hours]').textContent =
+        this.addLeadingZero(hours);
+      this.rootSelector.querySelector('[data-minutes]').textContent =
+        this.addLeadingZero(minutes);
+      this.rootSelector.querySelector('[data-seconds]').textContent =
+        this.addLeadingZero(seconds);
+    }, TIMER_DELAY);
+  },
+
+  stop() {
+    clearInterval(intervalId);
+    this.intervalId = null;
+    startBtn.disabled = true;
+    calendar.disabled = false;
+  },
+
+  convertMs(ms) {
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
+
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = this.addLeadingZero(
+      Math.floor(((ms % day) % hour) / minute)
+    );
+    const seconds = this.addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+
     return { days, hours, minutes, seconds };
-  }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+  },
+
+  addLeadingZero(value) {
+    return String(value).padStart(2, 0);
+  },
+};
